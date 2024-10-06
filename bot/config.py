bf6aab4396a94bfe3a0d0e20b8187828bf59fbd0
skarkii <lucas@kuja-halkola.com>
2024-10-06 08:03:06 +0200

    Added verification for all values in config.ini

--- bot/config.py
+++ bot/config.py
@@ -2,3 +2,18 @@ import configparser
 
 config = configparser.ConfigParser()
 config.read('config.ini')
+
+try:
+    list_of_existing_variables = {
+        config['DISCORD']['TOKEN'],
+        config['DISCORD']['OWNER_ID'],
+        config['DISCORD']['NOTIFY_NEW_VERSIONS'],
+        config['DISCORD']['IDLE_TIMEOUT'],
+        config['MESSAGES']['PREFIX'],
+        config['SPOTIFY']['ENABLED'],
+        config['SPOTIFY']['ID'],
+        config['SPOTIFY']['SECRET'],
+    }
+except KeyError as e:
+    print(f"Your config file is invalid you are missing:\n{e.args[0]}\nCheck example_config.ini to see what it does and add it!")
+    exit(1)
