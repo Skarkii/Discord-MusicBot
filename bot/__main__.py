fbbb79773200db4f44ff29ecf35d55b59e950b70
jlmoraleshellin <jlmoraleshellin@gmail.com>
2024-10-07 20:54:11 +0200

    Added configuration file verification before start

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,5 +1,5 @@
 from bot import *
-from config import config as config
+from config import config as config, check_config_values
 
 
 def main():
@@ -7,9 +7,13 @@ def main():
     intents.message_content = True
     intents.voice_states = True
     
-    TOKEN = config['DISCORD']['TOKEN']
-    
-    Bot(TOKEN, intents)
+    missing_values = check_config_values()
+    if len(missing_values) > 0:
+        print(f'Missing required options in config file: {missing_values}')
+    else:
+        TOKEN = config['DISCORD']['TOKEN']
+        print(TOKEN)
+        Bot(TOKEN, intents)
     
 if(__name__ == '__main__'):
     main()
--- bot/config.py
+++ bot/config.py
@@ -1,4 +1,20 @@
 import configparser
 
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read('bot\example_config.ini')
+
+REQUIRED_CONFIG = {
+                'DISCORD': ['TOKEN', 'OWNER_ID', 'NOTIFY_NEW_VERSIONS', 'IDLE_TIMOUT'],
+                'MESSAGES': ['PREFIX'],
+                'SPOTIFY': ['ENABLED', 'ID', 'SECRET']
+                }
+
+def check_config_values(sections: dict = REQUIRED_CONFIG):
+
+    missing_options = []
+    for section in sections:
+        for option in sections[section]:
+            if not config.has_option(section, option):
+                missing_options.append(option)
+
+    return missing_options
